010ff649e81da98fd2765244ac7245ce910e93e5
chayanghosh <59326645+chayanghosh@users.noreply.github.com>
2023-12-09 06:56:56 +0000

    TO-DO App created

---
+++ TodoApp/todo.js
@@ -0,0 +1,33 @@
+let c=0
+document.getElementById("add").addEventListener("click", () => {
+    let text=document.getElementById("newtask").value
+    if(text===''){
+        alert("This field can't be empty");
+    }
+    else{
+        document.getElementById("newtask").value=''
+        let list=document.createElement('li')
+        list.setAttribute("style","color: white; ")
+        list.setAttribute("id",`li${c}`)
+        list.innerHTML=`<span id=tasktext>${text}</span> &nbsp <button id="done">✅</button>
+                                <button id="delete" class="buttons">🗑️</button>`
+        document.getElementById("pendingTasks").appendChild(list)
+        c++
+        
+    }
+})
+
+document.getElementById("pendingTasks").addEventListener("click",(ev)=>{
+    if(ev.target.getAttribute('id') === "done"){
+        for(let childNode of ev.target.parentNode.childNodes){
+            if(childNode.getAttribute('id') === 'tasktext'){
+                childNode.classList.toggle('strike')
+                break
+            }
+        }   
+    }
+    else if(ev.target.getAttribute('id') === "delete"){
+        ev.target.parentNode.remove()
+    }
+
+})
